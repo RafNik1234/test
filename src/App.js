7751b0ab6e234797a19b98351516cd2914f44122
RafNik1234 <nikoghosyanrafayell@gmail.com>
2022-12-28 06:14:14 -0500

    feat: tinycolor add for production

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React цреате реацт апп шитх лоцксгенератион
         </a>
       </header>
     </div>
